932f3d50e9ea211005d2e20ca57af39da1be10a6
nick <32414396+qurle@users.noreply.github.com>
2023-07-10 20:41:01 +0300

    Initial commit

---
+++ code.ts
@@ -0,0 +1,70 @@
+// Constants
+const confirmMsgs = ["Done!", "You got it!", "Aye!", "Is that all?", "My job here is done.", "Gotcha!", "It wasn't hard.", "Got it! What's next?"]
+const renameMsgs = ["Cleaned", "Affected", "Made it with", "Fixed"]
+const idleMsgs = ["All great, already", "Nothing to do, everything's good", "Any layers to affect? Can't see it", "Nothing to do, your layers are great"]
+
+// Variables
+let notification: NotificationHandler
+let selection: ReadonlyArray<SceneNode>
+let working: boolean
+let count: number = 0
+
+figma.on("currentpagechange", cancel)
+
+// For networking purposes
+figma.showUI(__html__, { visible: false })
+const post = (k, v = 1, last = false) => figma.ui.postMessage({ k: k, v: v, last: last })
+figma.ui.onmessage = async (msg) => {
+  if (msg === "finished") // Real plugin finish (after server's last response)
+    figma.closePlugin()
+  else
+    console.log(msg)
+}
+
+// Main + Elements Check
+post("started")
+working = true
+selection = figma.currentPage.selection
+
+// Anything selected?
+if (selection.length)
+  for (const node of selection)
+    mainFunction(node)
+else
+  mainFunction(figma.currentPage)
+finish()
+
+// Action for selected nodes
+function mainFunction(node) {
+  count++
+}
+
+// Ending the work
+function finish() {
+  working = false
+  figma.root.setRelaunchData({ relaunch: '' })
+  if (count > 0) {
+    notify(confirmMsgs[Math.floor(Math.random() * confirmMsgs.length)] +
+      " " + renameMsgs[Math.floor(Math.random() * renameMsgs.length)] +
+      " " + ((count === 1) ? "only one layer" : (count + " layers")))
+
+  }
+  else notify(idleMsgs[Math.floor(Math.random() * idleMsgs.length)])
+  setTimeout(() => { console.log("Timeouted"), figma.closePlugin() }, 3000)
+}
+
+// Show new notification
+function notify(text: string) {
+  if (notification != null)
+    notification.cancel()
+  notification = figma.notify(text)
+}
+
+// Showing interruption notification
+function cancel() {
+  if (notification != null)
+    notification.cancel()
+  if (working) {
+    notify("Plugin work have been interrupted")
+  }
+}
